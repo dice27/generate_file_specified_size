bcacd3c968d0ee6141ef5e7bb0fccc55140c69b5
Yudai Takada <t.yudai92@gmail.com>
2018-07-22 21:57:08 +0900

    initial commit.

---
+++ generate_file_specified_size.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
+
+def write(path, val):
+    file = open(path, 'w')
+    lst = [chr(c) for c in range(48,58)+range(65,91)+range(97,123)]
+    for i in xrange(int(val)):
+        file.write(random.choice(lst).strip())
+    file.close
+
+with open('setting/path.conf', 'r') as fh:
+    for line in fh:
+        if line[0] == '#':
+            continue
+
+        path_val = line.strip().split("=")
+        path = path_val[0]
+        val = path_val[1]
+        write(path, val)
